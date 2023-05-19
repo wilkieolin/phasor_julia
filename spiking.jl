7a06012db729d62bc5a732ad1bcdd6de913fee71
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2023-05-19 14:04:24 -0500

    saving find spikes method to src

--- spiking.jl
+++ spiking.jl
@@ -92,9 +92,36 @@ function bias_current(bias::AbstractVecOrMat, t::Real, t_offset::Real, spk_args:
     return current
 end
 
-function find_spikes(sol, offset::Real, spk_args::SpikingArgs)
-    #TODO
-    return
+function find_spikes_rf(sol::ODESolution, spk_args::SpikingArgs)
+    t = sol.t
+    u = sol.u
+
+    @assert typeof(sol.u) <: Vector{<:Matrix{<:Complex}} "This method is for R&F neurons with complex potential"
+    #if potential is from an R&F neuron, it is complex and voltage is the imaginary part
+    voltage = imag.(u)
+    current = real.(u)
+
+    #convert vector of matrices to 2D matrix
+    make2d = x -> reduce(vcat, x)
+    voltage = make2d(voltage)
+    current = make2d(current)
+
+    #find the local voltage maxima through the first derivative (current)
+    maxima = findall(diff(sign.(current), dims=1) .< 0)
+    zero_i = current[maxima]
+    peak_voltages = voltage[maxima]
+    #return maxima, peak_voltages
+    #check voltages at these peaks are above the threshold
+    above_threshold = peak_voltages .> spk_args.threshold
+    spikes = maxima[above_threshold]
+
+    #retrieve the indices of the spiking neurons
+    channels = getindex.(spikes, 2)
+    #retrieve the times they spiked at
+    times = t[getindex.(spikes, 1)]
+    
+    return channels, times
+
 end
 
 function phase_to_train(phases::AbstractMatrix, spk_args::SpikingArgs, repeats::Int = 1, offset::Real = 0.0)
