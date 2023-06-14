e07ba02bc8ee98d8a98778834e865f6791bba348
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2023-06-14 13:45:31 -0400

    adding exports

--- module.jl
+++ module.jl
@@ -3,6 +3,32 @@ module PhasorNetworks
 using Flux, Functors, DifferentialEquations, Statistics
 
 export 
+#types
+PhasorDense, 
+SpikeTrain, 
+SpikingArgs, 
+SpikingCall, 
+CurrentCall,
+
+#spiking
+default_spk_args,
+phase_to_train,
+train_to_phase,
+zero_nans,
+cycle_correlation,
+cor_realvals,
+
+#vsa
+bundle,
+bundle_project,
+bind,
+angle_to_complex,
+complex_to_angle,
+random_symbols,
+similarity,
+similarity_self,
+similarity_outer,
+
 include("src/vsa.jl")
 include("src/spiking.jl")
 include("src/network.jl")
