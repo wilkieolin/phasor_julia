a59f38aa46ee3feb9918808e37c7f568055d3a9b
Wilkie Olin-Ammentorp <wolinamm@gmail.com>
2024-07-05 15:42:11 -0500

    merging training methods in distributed

--- train_classifier.jl
+++ train_classifier.jl
@@ -24,6 +24,120 @@ function momentum_to_label(pt, threshold::Real = 0.2)
     y = onehotbatch(get_truth(pt, threshold), (0, 1, 2))
     return y
 end
+
+###
+### Code for conventional multi-layer perceptron
+###
+
+function process_inputs_mlp(x, y_local)
+    x = scale_charge(x)
+    x = sum(x, dims=(1,3))
+    n_batch = size(x, 4)
+    x = reshape(x, (n_px, n_batch))
+    y_local = reshape(y_local, (1, n_batch))
+
+    x = cat(x, y_local, dims = 1)
+    return x
+end
+
+logitcrossentropy(y_pred, y) = mean(-1 * sum(y .* logsoftmax(y_pred); dims=1))
+
+function loss_mlp(x, xl, y, model, ps, st, threshold)
+    y_pred, st = model(process_inputs_mlp(x, xl), ps, st)
+    y = momentum_to_label(y, threshold)
+    loss = logitcrossentropy(y_pred, y)
+    return loss, st
+end
+
+function train_mlp(model, ps, st, train_loader, threshold::Real = 0.2; id::Int, kws...)
+    args = Args(; kws...) ## Collect options in a struct for convenience
+
+    device = cpu
+
+    @info "Constructing model and starting training"
+    ## Construct model
+    #model = build_model() |> device
+
+    ## Optimizer
+    opt_state = Optimisers.setup(Adam(3e-4), ps)
+    losses = []
+
+    ## Training
+    for epoch in 1:args.epochs
+        epoch_losses = []
+        for (x, xl, y) in train_loader
+            (loss_val, st), gs = withgradient(p -> loss(x, xl, y, model, p, st, threshold), ps)
+            append!(epoch_losses, loss_val)
+            opt_state, ps = Optimisers.update(opt_state, ps, gs[1]) ## update parameters
+        end
+        append!(losses, mean(epoch_losses))
+    end
+
+    filename = joinpath("parameters", "mlp_id_") * string(id) * "_epoch_" * string(args.epoch) * ".jld2"
+    jldsave(filename; params=ps, state=st)
+
+    jldsave(joinpath("parameters", "mlp_losses_id") * string(id) * ".jld2"; losses = losses)
+
+    return losses, ps, st
+end
+
+
+###
+### Code for phasor-based mlp
+###
+
+function process_inputs_pmlp(x, y_local)
+    x = scale_charge(x)
+    x = sum(x, dims=(1,3))
+    n_batch = size(x, 4)
+    x = reshape(x, (n_px, n_batch))
+    y_local = reshape(y_local, (1, n_batch))
+
+    x = cat(x, y_local, dims = 1)
+    return x
+end
+
+function loss_pmlp(x, xl, y, model, ps, st, threshold)
+    y_pred, st = model(process_inputs_pmlp(x, xl), ps, st)
+    y = momentum_to_label(y, threshold)
+    loss = quadrature_loss(y_pred, y) |> mean
+    return loss, st
+end
+
+function train_pmlp(model, ps, st, train_loader, threshold::Real = 0.2; id::Int, kws...)
+    args = Args(; kws...) ## Collect options in a struct for convenience
+
+    device = cpu
+
+    @info "Constructing model and starting training"
+    ## Construct model
+    #model = build_model() |> device
+
+    ## Optimizer
+    opt_state = Optimisers.setup(Adam(3e-4), ps)
+    losses = []
+
+    ## Training
+    for epoch in 1:args.epochs
+        epoch_losses = []
+        for (x, xl, y) in train_loader
+            (loss_val, st), gs = withgradient(p -> loss_pmlp(x, xl, y, model, p, st, threshold), ps)
+            append!(epoch_losses, loss_val)
+            opt_state, ps = Optimisers.update(opt_state, ps, gs[1]) ## update parameters
+        end
+        append!(losses, mean(epoch_losses))
+        filename = joinpath("parameters", "pmlp_id_") * string(id) * "_epoch_" * string(epoch) * ".jld2"
+        jldsave(filename; params=ps, state=st)
+    end
+
+    jldsave(joinpath("parameters", "pmlp_losses_id") * string(id) * ".jld2"; losses = losses)
+    return losses, ps, st
+end
+
+###
+### Code for analog, continuous-time driven phasor NN
+###
+
 function process_inputs_ode(x::AbstractArray, x_tms::AbstractVector, y_local::AbstractArray, spk_args::SpikingArgs)
     v_fn = t -> sum(scale_charge(interpolate_2D(t, x_tms, x)), dims=2)[:,1,:]
     y_fn = t -> ylocal_to_current(t, y_local, spk_args)
@@ -68,6 +182,8 @@ function train_ode(model, ps, st, train_loader, x_tms, threshold::Real = 0.2; id
         filename = joinpath("parameters", "ode_id_") * string(id) * "_epoch_" * string(epoch) * ".jld2"
         jldsave(filename; params=ps, state=st)
     end
+
+    jldsave(joinpath("parameters", "ode_losses_id") * string(id) * ".jld2"; losses = losses)
 
     return losses, ps, st
 end
---
+++ train_distributed.jl
@@ -0,0 +1,79 @@
+using Distributed
+n_procs = parse(Int, ARGS[1])
+type = ARGS[2]
+@assert type in ("mlp", "pmlp", "ode"), "Unrecognized network type requested"
+
+seeds = collect(43:43+n_procs)
+addprocs(n_procs)
+
+@everywhere include("train_classifier.jl")
+@everywhere n_epochs = 100
+
+@everywhere function exec_training(seed::Int)
+    global type
+    
+    data_dir = "pixel_data/"
+    file_pairs = get_dataset(data_dir)
+
+    q, ylocal, pt = get_samples(file_pairs[1:2]);
+    q_test, ylocal_test, pt_test = get_samples(file_pairs[3:3]);
+
+    args = Args(batchsize = 128)
+
+    test_loader = DataLoader((q_test, ylocal_test, pt_test), batchsize=args.batchsize)
+    train_loader = DataLoader((q, ylocal, pt), batchsize=args.batchsize)
+
+    x, xl, y = first(train_loader)
+    x_tms = range(start=0.0, stop=1.0, length=size(x, 1)) |> collect
+    n_px = size(x, 2) 
+    n_in = n_px + 1
+    sa = SpikingArgs()
+    rng = Xoshiro(seed)
+
+    if type == "ode"
+        ode_fn = Chain(BatchNorm(n_in),
+                        x -> tanh.(x),
+                        Dense(n_in => 128))
+
+
+        ode_model = Chain(PhasorODE(ode_fn, tspan=(0.0, 1.0), dt=0.01),
+                        x -> complex_to_angle(Array(x)[:,:,end]),
+                        PhasorDenseF32(128 => 3))
+
+        ps, st = Lux.setup(rng, ode_model)
+        psa = ComponentArray(ps)
+        @time lhist, pst, stt = train_ode(ode_model, psa, st, train_loader, x_tms, id=seed, epochs=n_epochs)
+
+    elseif type == "pmlp"
+        model = Chain(
+                        BatchNorm(n_in),
+                        x -> tanh.(x),
+                        PhasorDense(n_in => 128),
+                        PhasorDense(128 => 3) 
+                    )
+
+        ps, st = Lux.setup(rng, model)
+        psa = ComponentArray(ps)
+        @time lhist, pst, stt = train(model, ps, st, train_loader, id=seed, epochs=n_epochs)
+
+    elseif type == "mlp"
+        model = Chain(
+                    BatchNorm(n_in),
+                    x -> tanh.(x),
+                    Dense(n_in => 128, relu),
+                    Dense(128 => 3) 
+                    )
+        
+        ps, st = Lux.setup(rng, model)
+        psa = ComponentArray(ps)
+        @time lhist, pst, stt = train_mlp(model, psa, st, train_loader, id=seed, epochs=n_epochs)        
+    end
+
+    return 1
+end
+
+exec = pmap(exec_training, seeds)
+
+for i in workers()
+	rmprocs(i)
+end
